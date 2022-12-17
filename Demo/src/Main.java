b9a7c0bcd083c1fbdb922d4cef89a8c9c253b78a
helloloop <1204386811@qq.com>
2022-12-17 18:04:50 -0500

    init

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> al = new ArrayList<>();
+        int[] arr = {1, 2, 3};
+
+        System.out.println(al);
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.remove(1);
+        System.out.println(al);
+
     }
 }
